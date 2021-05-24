2acbdd74ded2cb9118f72cfc70ca6fd63adefd25
mitchbrunke <65658409+mitchbrunke@users.noreply.github.com>
2021-05-24 14:05:21 +1000

    plan your visit page

--- index.js
+++ index.js
@@ -9,3 +9,26 @@ mobileBtn.addEventListener("click", () => {
 mobileBtnExit.addEventListener("click", () => {
   nav.classList.remove("menu-btn");
 });
+
+function initAcc(elem, option) {
+  document.addEventListener("click", function (e) {
+    if (!e.target.matches(elem + " .a-btn")) return;
+    else {
+      if (!e.target.parentElement.classList.contains("active")) {
+        if (option == true) {
+          var elementList = document.querySelectorAll(elem + " .a-container");
+          Array.prototype.forEach.call(elementList, function (e) {
+            e.classList.remove("active");
+          });
+        }
+        e.target.parentElement.classList.add("active");
+        console.log("open");
+      } else {
+        e.target.parentElement.classList.remove("active");
+      }
+    }
+  });
+}
+
+initAcc(".accordion.v1", true);
+initAcc(".accordion.v2", false);
